daef3f389a2d97f7fcb724a7e468f8ec7cdc9b6b
Hunter Peeters <hunter_16-04@hunter-16.04>
2022-02-24 08:56:38 -0500

    Updated listener script with better naming convention

--- scripts/return_home.py
+++ scripts/return_home.py
@@ -7,22 +7,29 @@ from move_base_msgs.msg import MoveBaseActionGoal
 from multiprocessing import Process, Pipe
 import thread, time
 
+#flag used to ensure home goal is only sent once
 flag = False
 
+#ros return_home subscriber callback function
+#Checks if data has been published to move_base/cancel topic and changes flag
 def callback(data):
         if (data.id==''):
 		global flag
 		flag = True	
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
+#function to send the robot the origin as a goal when exploration is complete
+def return_home():
+    #initialize ros subscriber to move_base/cancel
+    rospy.init_node('return_home', anonymous=True)
     rospy.Subscriber('move_base/cancel', GoalID, callback)
+    #initialize ros publisher to move_base_simple/goal
     pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=100)
     rate = rospy.Rate(20)
-    # spin() simply keeps python from exiting until this node is stopped
+    #loop to keep the nodes going
     while not rospy.is_shutdown():
-    # do whatever you want here
+    #check is mapping is complete (flag)
 	if (flag==True):
+		#if mapping is complete, let user know and then return to home
 		print("EXPLORATION STOPPED")
 		goal = PoseStamped()
 		goal.header.stamp=rospy.get_rostime()
@@ -39,6 +46,6 @@ def listener():
 
 if __name__ == '__main__':
     try:
-        listener()
+        return_home()
     except rospy.ROSInterruptException:
         pass
