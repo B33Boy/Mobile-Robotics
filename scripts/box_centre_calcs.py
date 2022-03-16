13bb785e4af444844178da88a2c8385b11f6986c
Hunter Peeters <hunter_16-04@hunter-16.04>
2022-03-15 22:06:33 -0400

    Completed roomba algorithm integration, and return home publish

--- scripts/box_centre_calcs.py
+++ scripts/box_centre_calcs.py
@@ -33,85 +33,86 @@ def callback(data):
 #function to send the robot the origin as a goal when exploration is complete
 def box_locator():
     #initialize ros subscriber to move_base/cancel
-    rospy.init_node('box_locator', anonymous=True)
-    rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback)
+	rospy.init_node('box_locator', anonymous=True)
+	rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback)
     #initialize ros publisher to move_base_simple/goal
-    pub1 = rospy.Publisher('visualization_marker_1', Marker, queue_size=100)
-    pub2 = rospy.Publisher('visualization_marker_2', Marker, queue_size=100)
+	pub1 = rospy.Publisher('visualization_marker_1', Marker, queue_size=100)
+	pub2 = rospy.Publisher('visualization_marker_2', Marker, queue_size=100)
 	pub_counter = rospy.Publisher('box_counter', Int32, queue_size=10)
-    rate = rospy.Rate(30)
+	rate = rospy.Rate(30)
     #loop to keep the nodes going
-    while not rospy.is_shutdown():
+	while not rospy.is_shutdown():
     #check is mapping is complete (flag)
-	global boxInfo
-	if (boxInfo[0]==0 or boxInfo[0]==1):
-		global packageOneFlag
-		if (packageOneFlag==False):
-			packageOneFlag = True
-			marker1=Marker()
-			marker1.header.stamp=rospy.get_rostime()
-			marker1.header.frame_id='map'
-			marker1.type=1
-			marker1.action=0
-			marker1.pose.position.x = boxInfo[1]
-			marker1.pose.position.y = boxInfo[2]
-			marker1.pose.position.z = boxInfo[3]
-			if (boxInfo[0]==0):
-				marker1.scale.x=0.10
-				marker1.scale.y=0.05
-				marker1.scale.z=0.05
-			if (boxInfo[0]==1):
-				marker1.scale.x=0.05
-				marker1.scale.y=0.05
-				marker1.scale.z=0.10
-			marker1.pose.orientation.x=Q[0]
-			marker1.pose.orientation.y=Q[1]
-			marker1.pose.orientation.z=Q[2]
-			marker1.pose.orientation.w=Q[3]
-			marker1.color.g=0
-			marker1.color.r=0
-			marker1.color.b=1
-			marker1.color.a=1
-			pub1.publish(marker1)
+		global boxInfo
+		if (boxInfo[0]==0 or boxInfo[0]==1):
+			global packageOneFlag
+			if (packageOneFlag==False):
+				packageOneFlag = True
+				marker1=Marker()
+				marker1.header.stamp=rospy.get_rostime()
+				marker1.header.frame_id='map'
+				marker1.type=1
+				marker1.action=0
+				marker1.pose.position.x = boxInfo[1]
+				marker1.pose.position.y = boxInfo[2]
+				marker1.pose.position.z = boxInfo[3]
+				if (boxInfo[0]==0):
+					marker1.scale.x=0.10
+					marker1.scale.y=0.05
+					marker1.scale.z=0.05
+				if (boxInfo[0]==1):
+					marker1.scale.x=0.05
+					marker1.scale.y=0.05
+					marker1.scale.z=0.10
+				marker1.pose.orientation.x=Q[0]
+				marker1.pose.orientation.y=Q[1]
+				marker1.pose.orientation.z=Q[2]
+				marker1.pose.orientation.w=Q[3]
+				marker1.color.g=0
+				marker1.color.r=0
+				marker1.color.b=1
+				marker1.color.a=1
+				pub1.publish(marker1)
 			
 			
-	elif (boxInfo[0]==2 or boxInfo[0]==3):
-		global packageTwoFlag
-		if (packageTwoFlag==False):
-			packageTwoFlag = True
-			marker2=Marker()
-			marker2.header.stamp=rospy.get_rostime()
-			marker2.header.frame_id='map'
-			marker2.type=1
-			marker2.action=0
-			marker2.pose.position.x = boxInfo[1]
-			marker2.pose.position.y = boxInfo[2]
-			marker2.pose.position.z = boxInfo[3]
-			if (boxInfo[0]==2):
-				marker2.scale.x=0.10
-				marker2.scale.y=0.05
-				marker2.scale.z=0.05
-			if (boxInfo[0]==3):
-				marker2.scale.x=0.05
-				marker2.scale.y=0.05
-				marker2.scale.z=0.10
-			marker2.pose.orientation.x=Q[0]
-			marker2.pose.orientation.y=Q[1]
-			marker2.pose.orientation.z=Q[2]
-			marker2.pose.orientation.w=Q[3]
-			marker2.color.g=0
-			marker2.color.r=1
-			marker2.color.b=0
-			marker2.color.a=1
-			pub2.publish(marker2)
-	
-	if (packageOneFlag==False and packageTwoFlag==False):
-		counter = 0	
-	if (packageOneFlag==True or packageTwoFlag==True):
-		counter = 1
-	if (packageOneFlag==True and packageTwoFlag==True):
-		counter = 2
-	pub_counter.publish(counter)
+		elif (boxInfo[0]==2 or boxInfo[0]==3):
+			global packageTwoFlag
+			if (packageTwoFlag==False):
+				packageTwoFlag = True
+				marker2=Marker()
+				marker2.header.stamp=rospy.get_rostime()
+				marker2.header.frame_id='map'
+				marker2.type=1
+				marker2.action=0
+				marker2.pose.position.x = boxInfo[1]
+				marker2.pose.position.y = boxInfo[2]
+				marker2.pose.position.z = boxInfo[3]
+				if (boxInfo[0]==2):
+					marker2.scale.x=0.10
+					marker2.scale.y=0.05
+					marker2.scale.z=0.05
+				if (boxInfo[0]==3):
+					marker2.scale.x=0.05
+					marker2.scale.y=0.05
+					marker2.scale.z=0.10
+				marker2.pose.orientation.x=Q[0]
+				marker2.pose.orientation.y=Q[1]
+				marker2.pose.orientation.z=Q[2]
+				marker2.pose.orientation.w=Q[3]
+				marker2.color.g=0
+				marker2.color.r=1
+				marker2.color.b=0
+				marker2.color.a=1
+				pub2.publish(marker2)
+
+		counter=0
+		if (packageOneFlag==False and packageTwoFlag==False):
+			counter = 0	
+		if (packageOneFlag==True or packageTwoFlag==True):
+			counter = 1
+		if (packageOneFlag==True and packageTwoFlag==True):
+			counter = 2
+		pub_counter.publish(counter)
 
 	rate.sleep()
 
--- scripts/search_and_return_home.py
+++ scripts/search_and_return_home.py
@@ -4,6 +4,7 @@ from geometry_msgs.msg import PoseStamped
 from actionlib_msgs.msg import GoalID
 from sensor_msgs.msg import LaserScan # LaserScan type message is defined in sensor_msgs
 from geometry_msgs.msg import Twist #
+from std_msgs.msg import Int32
 
 #flag used to ensure home goal is only sent once
 flag = False
@@ -19,47 +20,43 @@ def callback_returnHome(data):
 
 def callback_roomba(msg):
 	global move
-    thr1 = 0.4 # Laser scan range threshold
-    thr2 = 0.4
-    if msg.ranges[0]>thr1 and msg.ranges[15]>thr2 and msg.ranges[345]>thr2: # Checks if there are obstacles in front and
+	thr1 = 0.4 # Laser scan range threshold
+	thr2 = 0.4
+	if msg.ranges[0]>thr1 and msg.ranges[15]>thr2 and msg.ranges[345]>thr2: # Checks if there are obstacles in front and
                                                                          # 15 degrees left and right (Try changing the
 									 # the angle values as well as the thresholds)
-        move.linear.x = 0.15 # go forward (linear velocity)
-        move.angular.z = 0.0 # do not rotate (angular velocity)
-    else:
-        move.linear.x = 0.0 # stop
-        move.angular.z = 0.5 # rotate counter-clockwise
-        if msg.ranges[0]>thr1 and msg.ranges[15]>thr2 and msg.ranges[345]>thr2:
-            move.linear.x = 0.15
-            move.angular.z = 0.0
+		move.linear.x = 0.15 # go forward (linear velocity)
+		move.angular.z = 0.0 # do not rotate (angular velocity)
+	else:
+		move.linear.x = 0.0 # stop
+		move.angular.z = 0.5 # rotate counter-clockwise
+		if msg.ranges[0]>thr1 and msg.ranges[15]>thr2 and msg.ranges[345]>thr2:
+			move.linear.x = 0.15
+			move.angular.z = 0.0
 
-def callback_counter(data):
+def callback_counter(data_1):
 	global box_counter
-	box_counter = data
+	box_counter = data_1.data
 
 #function to send the robot the origin as a goal when exploration is complete
 def return_home():
     #initialize ros subscriber to move_base/cancel
-    rospy.init_node('search_and_return_home', anonymous=True)
-    rospy.Subscriber('move_base/cancel', GoalID, callback_returnHome)
-    #initialize ros publisher to move_base_simple/goal
-    pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=100)
+	rospy.init_node('search_and_return_home', anonymous=True)
+	rospy.Subscriber('move_base/cancel', GoalID, callback_returnHome)
+	#initialize ros publisher to move_base_simple/goal
+	pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=100)
 	pub_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)  # Publisher object which will publish "Twist" type messages
-                            				 # on the "/cmd_vel" Topic, "queue_size" is the size of the
-                                                         # outgoing message queue used for asynchronous publishing
-
 	sub = rospy.Subscriber("/scan", LaserScan, callback_roomba)  # Subscriber object which will listen "LaserScan" type messages
 	sub_counter = rospy.Subscriber("/box_counter", Int32, callback_counter)
-                                                      # from the "/scan" Topic and call the "callback" function
-						      # each time it reads something from the Topic
-    rate = rospy.Rate(20)
+
+	rate = rospy.Rate(0.1)
     #loop to keep the nodes going
-    while not rospy.is_shutdown():
+	while not rospy.is_shutdown():
     #check is mapping is complete (flag)
 		global flag, move, box_counter
 		if (flag==True and box_counter<2):
-	    	pub_vel.publish(move) # publish the move object
-		if (box_counter>1):
+			pub_vel.publish(move) # publish the move object
+		if (flag==True and box_counter>1):
 			print("SEARCH COMPLETE")
 			goal = PoseStamped()
 			goal.header.stamp=rospy.get_rostime()
@@ -70,7 +67,7 @@ def return_home():
 			goal.pose.orientation.w=1.0
 			rospy.loginfo(goal)
 			pub.publish(goal)
-		flag = False   	
+			flag = False   	
 	rate.sleep()
 
 if __name__ == '__main__':
