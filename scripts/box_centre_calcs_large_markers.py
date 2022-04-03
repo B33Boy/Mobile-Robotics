83fb18e2b4e26b1f2396f86a83e655460fa8621d
Hunter Peeters <hunter_16-04@hunter-16.04>
2022-04-03 12:50:16 -0400

    new file to make markers larger and added one scan topic per box

---
+++ scripts/box_centre_calcs_large_markers.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String, Int32
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from visualization_msgs.msg import Marker
+
+# Flag used to ensure home goal is only sent once
+packageOneFlag = False
+packageTwoFlag = False
+Q = [0,0,0,0]
+boxInfo = [5,5,5,5]
+#boxReference = {'ids': 0,1,2,3, 'width': 10,5,10,5, 'height': 5,5,5,5, 'depth': 5,10,5,10}
+
+def callback(data):
+	"""
+    Callback function to check and parse the aruco markers' pose with respect to the camera
+ 
+    Input
+    :param data: data of the message received on a topic that is of hierarchial structure
+ 
+    Output
+    :return: 
+    """
+
+	if(data.markers[0].id!=''):
+		global boxInfo
+		boxInfo[0] = data.markers[0].id
+		boxInfo[1] = data.markers[0].pose.pose.position.x
+		boxInfo[2] = data.markers[0].pose.pose.position.y
+		boxInfo[3] = data.markers[0].pose.pose.position.z
+		global Q
+		Q[0] = data.markers[0].pose.pose.orientation.x
+		Q[1] = data.markers[0].pose.pose.orientation.y
+		Q[2] = data.markers[0].pose.pose.orientation.z
+		Q[3] = data.markers[0].pose.pose.orientation.w	
+
+def publish_marker_wrt_map(boxInfo, Q, boxTopID, boxSideID, pub_topic, color=[0,0,0,1]):
+
+	marker=Marker()
+	marker.header.stamp=rospy.get_rostime()
+
+	# Ensure that the marker is plotted wrt to map frame
+	marker.header.frame_id='map'
+
+	# Indicates marker is a cube
+	marker.type=1
+
+	# Indicates that we want to add/modify object
+	marker.action=0
+	# Set position of box
+	marker.pose.position.x = boxInfo[1]
+	marker.pose.position.y = boxInfo[2]
+	marker.pose.position.z = boxInfo[3]
+
+	# Scale box side lengths depending on top or side marker 
+	if (boxInfo[0]==boxTopID):
+		marker.scale.x=0.2
+		marker.scale.y=0.2
+		marker.scale.z=0.2
+
+	if (boxInfo[0]==boxSideID):
+		marker.scale.x=0.2
+		marker.scale.y=0.2
+		marker.scale.z=0.2
+
+	# Set orientation of box
+	marker.pose.orientation.x=Q[0]
+	marker.pose.orientation.y=Q[1]
+	marker.pose.orientation.z=Q[2]
+	marker.pose.orientation.w=Q[3]
+
+	marker.color.g=color[0]
+	marker.color.r=color[1]
+	marker.color.b=color[2]
+	marker.color.a=color[3]
+
+	pub_topic.publish(marker)
+
+
+#function to send the robot the origin as a goal when exploration is complete
+def box_locator():
+
+	"""
+    Function to create ROS nodes to utilize AlvarMarkers to check and parse the aruco markers' pose with respect to the camera
+
+    Output
+    :return: returns nothing
+    """
+
+    # Initialize ROS node called box_locator
+	rospy.init_node('box_locator', anonymous=True)
+
+	# Initialize subscriber to ar_pose_marker topic and call the callback function when data arrives
+	rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback)
+	
+	# Initialize ROS publisher to publish pose of markers
+	pub1 = rospy.Publisher('visualization_marker_1', Marker, queue_size=100)
+	pub2 = rospy.Publisher('visualization_marker_2', Marker, queue_size=100)
+	
+	# Initialize ROS publisher to publish number of boxes detected
+	pub_counter = rospy.Publisher('box_counter', Int32, queue_size=10)
+	
+	rate = rospy.Rate(30)
+    
+	while not rospy.is_shutdown():
+		global boxInfo
+
+		# Check if the 0 or 1 marker ID (corresponding to 1st package) is detected 
+		if (boxInfo[0]==0 or boxInfo[0]==1):
+			global packageOneFlag
+
+			# Set the packageOneFlag to True to ensure detection only happens once 
+			if (packageOneFlag==False):
+				packageOneFlag = True
+
+				publish_marker_wrt_map(boxInfo, Q, 0, 1, pub1, [0,0,1,1])
+
+			
+		# Check if the 2 or 3 marker ID (corresponding to 2nd package) is detected 	
+		elif (boxInfo[0]==2 or boxInfo[0]==3):
+			global packageTwoFlag
+
+			# Set the packageTwoFlag to True to ensure detection only happens once 
+			if (packageTwoFlag==False):
+				packageTwoFlag = True
+
+				publish_marker_wrt_map(boxInfo, Q, 2, 3, pub2, [0,1,0,1])
+
+
+		counter=0
+		# Set counter depending on how many packages are found
+		if (packageOneFlag==False and packageTwoFlag==False):
+			counter = 0	
+		
+		if (packageOneFlag==True or packageTwoFlag==True):
+			counter = 1
+		
+		if (packageOneFlag==True and packageTwoFlag==True):
+			counter = 2
+		
+		pub_counter.publish(counter)
+
+		rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        box_locator()
+    except rospy.ROSInterruptException:
+        pass
--- scripts/costmap_updater.py
+++ scripts/costmap_updater.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
+from operator import truediv
 import rospy, math
 from visualization_msgs.msg import Marker
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from sensor_msgs.msg import LaserScan
 
 
-box=[0,19,6]
-time=[0.0,0.0]
+box1=[0,19,6]
+time1=[0.0,0.0]
+box2=[0,19,6]
+time2=[0.0,0.0]
+flag1 = True
+flag2 = True
 
 # Callback to save package boundaries and timestamp of scan
 def callback(data):
-	global box, time
-	box[0]=math.atan2(data.markers[0].pose.pose.position.y,data.markers[0].pose.pose.position.x)
-	box[1]=math.sqrt(data.markers[0].pose.pose.position.x*data.markers[0].pose.pose.position.x+data.markers[0].pose.pose.position.y*data.markers[0].pose.pose.position.y)
-	box[2]=data.markers[0].id
-	time[0]=data.markers[0].header.stamp.secs
-	time[1]=data.markers[0].header.stamp.nsecs
-	
+	global box1, time1, box2, time2, flag1, flag2
+	print(data.markers[0].id)
+	if (data.markers[0].id==0 or data.markers[0].id==1 and flag1):
+		box1[0]=math.atan2(data.markers[0].pose.pose.position.y,data.markers[0].pose.pose.position.x)
+		box1[1]=math.sqrt(data.markers[0].pose.pose.position.x*data.markers[0].pose.pose.position.x+data.markers[0].pose.pose.position.y*data.markers[0].pose.pose.position.y)
+		box1[2]=data.markers[0].id
+		time1[0]=data.markers[0].header.stamp.secs
+		time1[1]=data.markers[0].header.stamp.nsecs
+		flag1 = False
+	if (data.markers[0].id==2 or data.markers[0].id==3 and flag2):
+		box2[0]=math.atan2(data.markers[0].pose.pose.position.y,data.markers[0].pose.pose.position.x)
+		box2[1]=math.sqrt(data.markers[0].pose.pose.position.x*data.markers[0].pose.pose.position.x+data.markers[0].pose.pose.position.y*data.markers[0].pose.pose.position.y)
+		box2[2]=data.markers[0].id
+		time2[0]=data.markers[0].header.stamp.secs
+		time2[1]=data.markers[0].header.stamp.nsecs
+		flag2 = False
 
 # Function to update costmap 
 def costmap_updater():
@@ -29,26 +43,44 @@ def costmap_updater():
 
     # Initialize publisher to scan_1 topic
 	pub1 = rospy.Publisher('scan_1', LaserScan, queue_size=10)
+	pub2 = rospy.Publisher('scan_2', LaserScan, queue_size = 10)
 	
 	rate = rospy.Rate(1)
     
 	while not rospy.is_shutdown():
-		global box, time
+		global box1, time1, box2, time2
 		
 		# Create a LaserScan message and publish to box1_scan topic
 		box1_scan=LaserScan()
-		box1_scan.header.stamp.secs=time[0]
-		box1_scan.header.stamp.nsecs=time[1]
+		box1_scan.header.stamp.secs=time1[0]
+		box1_scan.header.stamp.nsecs=time1[1]
 		box1_scan.header.frame_id='map'
-		box1_scan.angle_min=box[0]
-		box1_scan.angle_max=box[0]+0.0001
+		#box1_scan.angle_min=box1[0]-0.015
+		#box1_scan.angle_max=box1[0]+0.015
+		box1_scan.angle_min=box1[0]
+		box1_scan.angle_max=box1[0]+0.0001
 		box1_scan.range_min=0
 		box1_scan.range_max=20
 		box1_scan.angle_increment=0.0001
 		box1_scan.time_increment=0.0001
-		box1_scan.ranges=[box[1],box[1]]
-
+		#box1_scan.ranges=[box1[1]+0.025,box1[1]+0.025,box1[1]+0.025]
+		box1_scan.ranges=[box1[1]+0.025,box1[1]+0.025]
 		pub1.publish(box1_scan)
+
+		box2_scan=LaserScan()
+		box2_scan.header.stamp.secs=time2[0]
+		box2_scan.header.stamp.nsecs=time2[1]
+		box2_scan.header.frame_id='map'
+		#box2_scan.angle_min=box2[0]-0.015
+		#box2_scan.angle_max=box2[0]+0.015
+		box2_scan.angle_min=box2[0]
+		box2_scan.angle_max=box2[0]+0.0001
+		box2_scan.range_min=0
+		box2_scan.range_max=20
+		box2_scan.angle_increment=0.015
+		box2_scan.time_increment=0.0001
+		box2_scan.ranges=[box2[1]+0.025,box2[1]+0.025]
+		pub2.publish(box2_scan)
 			
 	rate.sleep()
 
