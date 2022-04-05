de53e9c9ed6c1f6fb08d969a4eb67462885569b3
Hunter Peeters <hunter_16-04@hunter-16.04>
2022-04-05 12:23:46 -0400

    more comments again

--- src/PathPlanningNode.cpp
+++ src/PathPlanningNode.cpp
@@ -13,47 +13,38 @@ using std::string;
 using cm::Costmap2D;
 using cm::Costmap2DROS;
 
+//Flags to suppress the path planning node until exploration complete
 bool mappingFinished = true;
-bool counterFlag = true;
 
-
+//Function to change mappingFinished flag to false once mapping is finished
 void movecallback(const actionlib_msgs::GoalID moveCancelled){
   if(moveCancelled.id == ""){
     mappingFinished = false;
   }
 }
 
-void counterback(const std_msgs::Int32 numBoxesDetected){
-  if (numBoxesDetected.data < 2){
-    counterFlag = false;
-  }
-}
-
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "path_planning_node");//hjr
+  ros::init(argc, argv, "path_planning_node");
+  //Stay in the while loop until mapping is finished. Suppresses path planning until mapping finished
   while (mappingFinished){
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("move_base/cancel", 10, movecallback);
-    ros::Subscriber sub1 = n.subscribe("box_counter", 10, counterback);
   }
-  tf::TransformListener tf(ros::Duration(10));
-  //Run once this activates.
-           
+  tf::TransformListener tf(ros::Duration(10));      
   costmap_2d::Costmap2DROS lcr("cleaning_costmap", tf);
-  //planner_costmap_ros_->pause();
 
   ros::Duration(5).sleep();
   CleaningPathPlanning clr(&lcr);
   clr.GetPathInROS();
-  //clr.GetBorderTrackingPathInROS();
+
   ros::Rate r(1);
   while(ros::ok()){    
     clr.PublishCoveragePath();
     ros::spinOnce();
     r.sleep();
   }
-  ros::shutdown();//关闭节点以及所有与之相关的发布，订阅，调用与服务。
+  ros::shutdown();
   return 0;
 }
 
