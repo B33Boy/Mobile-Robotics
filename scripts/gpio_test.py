a6d699631ea58a315057e7c8fa580654f5062566
Hunter Peeters <hunter_16-04@hunter-16.04>
2022-04-12 21:47:36 -0400

    Added ros while loop

--- scripts/gpio_test.py
+++ scripts/gpio_test.py
@@ -20,8 +20,9 @@ if __name__ == '__main__':
     try:
         rospy.init_node('servo_control', anonymous=True)
         rospy.Subscriber('servo_angle', Float32, angleCallback)
-        #p.start(0)
-        SetAngle()
-        rospy.spin()
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            SetAngle()
+            rate.sleep()
     except rospy.ROSInterruptException:
         pass
